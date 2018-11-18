35ced09136838bd88672fa718504717639326bf6
qwake1378 <45040429+qwake1378@users.noreply.github.com>
2018-11-19 01:55:09 +0300

    Добавлена анимация плавного появления маленьких кнопок у главной.

--- app/src/main/java/com/example/kolvir/personalassistant_10/MainActivity.java
+++ app/src/main/java/com/example/kolvir/personalassistant_10/MainActivity.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     ImageButton menuButton, b1, b2, b3;
     boolean openMenuButton;
+    Animation buttonAnimation;
+    ImageView pic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,21 @@ public class MainActivity extends AppCompatActivity {
         b2 = (ImageButton) findViewById(R.id.button2);
         b3 = (ImageButton) findViewById(R.id.button3);
 
+
+        //buttonAnimation = AnimationUtils.loadAnimation(this,R.anim.);
+
+        buttonAnimation = AnimationUtils.loadAnimation(this,R.anim.movebutton);
+
+
         openMenuButton = false;
+
+        openMenuButton = false;
+        b1.setEnabled(false);
+        b2.setEnabled(false);
+        b3.setEnabled(false);
+        b1.setVisibility(View.INVISIBLE);
+        b2.setVisibility(View.INVISIBLE);
+        b3.setVisibility(View.INVISIBLE);
 
         View.OnClickListener oclBtnOk = new View.OnClickListener() {
             @Override
@@ -46,6 +65,9 @@ public class MainActivity extends AppCompatActivity {
                     if (openMenuButton) {
                         menuButton.setImageResource(R.drawable.kek);
                         openMenuButton = false;
+                        b1.clearAnimation();
+                        b2.clearAnimation();
+                        b3.clearAnimation();
                         b1.setEnabled(false);
                         b2.setEnabled(false);
                         b3.setEnabled(false);
@@ -59,8 +81,11 @@ public class MainActivity extends AppCompatActivity {
                         b2.setEnabled(true);
                         b3.setEnabled(true);
                         b1.setVisibility(View.VISIBLE);
+                        b1.startAnimation(buttonAnimation);
                         b2.setVisibility(View.VISIBLE);
+                        b2.startAnimation(buttonAnimation);
                         b3.setVisibility(View.VISIBLE);
+                        b3.startAnimation(buttonAnimation);
                     }
 
                 }
