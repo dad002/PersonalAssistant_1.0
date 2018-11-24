07add24fc4abd6db6bf4641fb54f296c8a8bad46
qwake1378 <45040429+qwake1378@users.noreply.github.com>
2018-11-24 15:34:19 +0300

    Добавлена анимация плавного появления маленьких кнопок у главной.

--- app/src/main/java/com/example/kolvir/personalassistant_10/MainActivity.java
+++ app/src/main/java/com/example/kolvir/personalassistant_10/MainActivity.java
@@ -5,95 +5,33 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    ImageButton menuButton, b1, b2, b3;
-    boolean openMenuButton;
-    Animation buttonAnimation;
-    ImageView pic;
-
+    MenuButton menuButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        menuButton = (ImageButton) findViewById(R.id.buttonMenu);
-        b1 = (ImageButton) findViewById(R.id.button1);
-        b2 = (ImageButton) findViewById(R.id.button2);
-        b3 = (ImageButton) findViewById(R.id.button3);
-
-
-        //buttonAnimation = AnimationUtils.loadAnimation(this,R.anim.);
-
-        buttonAnimation = AnimationUtils.loadAnimation(this,R.anim.movebutton);
-
-
-        openMenuButton = false;
-
-        openMenuButton = false;
-        b1.setEnabled(false);
-        b2.setEnabled(false);
-        b3.setEnabled(false);
-        b1.setVisibility(View.INVISIBLE);
-        b2.setVisibility(View.INVISIBLE);
-        b3.setVisibility(View.INVISIBLE);
-
-        View.OnClickListener oclBtnOk = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Меняем текст в TextView (tvOut)
-                Log.i("MYLOG", "PRESS");
-            }
-        };
+       menuButton = new MenuButton(this,(ImageButton)findViewById(R.id.buttonMenu),(ImageButton)findViewById(R.id.buttonTop),(ImageButton)findViewById(R.id.buttonMid),(ImageButton)findViewById(R.id.buttonBot));
 
         View.OnTouchListener otlBtnOk = new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    Log.i("MYLOG", "TouchDown");
-                    menuButton.setImageResource(R.drawable.kek2);
+                    menuButton.Up();
                 }
                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                    Log.i("MYLOG", "TouchUp");
-
-                    if (openMenuButton) {
-                        menuButton.setImageResource(R.drawable.kek);
-                        openMenuButton = false;
-                        b1.clearAnimation();
-                        b2.clearAnimation();
-                        b3.clearAnimation();
-                        b1.setEnabled(false);
-                        b2.setEnabled(false);
-                        b3.setEnabled(false);
-                        b1.setVisibility(View.INVISIBLE);
-                        b2.setVisibility(View.INVISIBLE);
-                        b3.setVisibility(View.INVISIBLE);
-                    } else {
-                        menuButton.setImageResource(R.drawable.kek3);
-                        openMenuButton = true;
-                        b1.setEnabled(true);
-                        b2.setEnabled(true);
-                        b3.setEnabled(true);
-                        b1.setVisibility(View.VISIBLE);
-                        b1.startAnimation(buttonAnimation);
-                        b2.setVisibility(View.VISIBLE);
-                        b2.startAnimation(buttonAnimation);
-                        b3.setVisibility(View.VISIBLE);
-                        b3.startAnimation(buttonAnimation);
-                    }
-
+                   menuButton.Tap();
                 }
                 return false;
             }
         };
 
-        menuButton.setOnTouchListener(otlBtnOk);
+        menuButton.menuButton.setOnTouchListener(otlBtnOk);
 
     }
 
---
+++ app/src/main/java/com/example/kolvir/personalassistant_10/MenuButton.java
@@ -0,0 +1,76 @@
+package com.example.kolvir.personalassistant_10;
+
+import android.content.Context;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MenuButton {
+
+    ImageButton menuButton, btnTop, btnMid, btnBot;
+    boolean openMenuButton;
+    Animation buttonAnimationTop,buttonAnimationMid,buttonAnimationBot;
+    ImageView pic;
+
+    public MenuButton(Context context,ImageButton main,ImageButton top,ImageButton mid,ImageButton bot){
+
+        menuButton = main;
+        btnTop = top;
+        btnMid = mid;
+        btnBot = bot;
+
+        //buttonAnimation = AnimationUtils.loadAnimation(this,R.anim.);
+
+        buttonAnimationTop= AnimationUtils.loadAnimation(context,R.anim.move_top_button);
+        buttonAnimationMid = AnimationUtils.loadAnimation(context,R.anim.move_mid_button);
+        buttonAnimationBot = AnimationUtils.loadAnimation(context,R.anim.move_bottom_button);
+
+
+        openMenuButton = false;
+
+        openMenuButton = false;
+        btnTop.setEnabled(false);
+        btnMid.setEnabled(false);
+        btnBot.setEnabled(false);
+        btnTop.setVisibility(View.INVISIBLE);
+        btnMid.setVisibility(View.INVISIBLE);
+        btnBot.setVisibility(View.INVISIBLE);
+    }
+
+    public void Up(){
+        menuButton.setImageResource(R.drawable.mainbtnon);
+    }
+
+    public void Tap(){
+        if (openMenuButton) {
+            menuButton.setImageResource(R.drawable.mainbtnoff);
+            openMenuButton = false;
+            btnTop.clearAnimation();
+            btnMid.clearAnimation();
+            btnBot.clearAnimation();
+            btnTop.setEnabled(false);
+            btnMid.setEnabled(false);
+            btnBot.setEnabled(false);
+            btnTop.setVisibility(View.INVISIBLE);
+            btnMid.setVisibility(View.INVISIBLE);
+            btnBot.setVisibility(View.INVISIBLE);
+        } else {
+            menuButton.setImageResource(R.drawable.mainbtnopen);
+            openMenuButton = true;
+            btnTop.setEnabled(true);
+            btnMid.setEnabled(true);
+            btnBot.setEnabled(true);
+            btnTop.setVisibility(View.VISIBLE);
+            btnTop.startAnimation(buttonAnimationTop);
+            btnMid.setVisibility(View.VISIBLE);
+            btnMid.startAnimation(buttonAnimationMid);
+            btnBot.setVisibility(View.VISIBLE);
+            btnBot.startAnimation(buttonAnimationBot);
+        }
+    }
+
+
+
+}
